e33f208a16af96e9b8b3231a011fcb2274260da3
Natan Lancha <natanlancha@gmail.com>
2020-02-08 14:07:29 +0100

    Modificacion de boton guardar. Sigo sin captarlo....

--- js/java.js
+++ js/java.js
@@ -3,63 +3,56 @@ var listaTareas = new Array();
 
 listaTareas = [
 	{
-		'idTarea': 0,
-		'titulo': 'Salir a comer',
-		'prioridad': 'diaria'
+		id: 0,
+		titulo: 'Salir a comer',
+		prioridad: 'diaria'
 
 	},
 	{
-		'idTarea': 1,
-		'titulo': 'Javascript',
-		'prioridad': 'urgente'
+		idTarea: 1,
+		titulo: 'Javascript',
+		prioridad: 'urgente'
 	},
 	{
-		'idTarea': 2,
-		'titulo': 'Dormir',
-		'prioridad': 'diaria'
+		idTarea: 2,
+		titulo: 'Dormir',
+		prioridad: 'diaria'
 	},
 	{
-		'idTarea': 3,
-		'titulo': 'Estudiar',
-		'prioridad': 'diario'
+		idTarea: 3,
+		titulo: 'Estudiar',
+		prioridad: 'diario'
 	},
 	{
-		'idTarea': 4,
-		'titulo': 'Compra',
-		'prioridad': 'mensual'
+		idTarea: 4,
+		titulo: 'Compra',
+		prioridad: 'mensual'
 	}
 ];
 
 var seccionTareas = document.getElementById('tareas')
 var mensaje = document.getElementById('mensaje')
-var mensajeFiltro = document.getElementById('mensajeFiltro')
-var tituloTarea = document.getElementById('tituloTarea')
-var btnGuardar = document.getElementById('botonGuardar')
+var btnGuardar = document.getElementById('guardar')
 var prioridad = document.getElementById('prioridad')
-var busqueda = document.querySelector('#search')
-
-var seleccionarPrioridad = document.getElementById('prioridadSelect')
-
-var eliminar
 
 var ultimoId = 1
 
 btnGuardar.addEventListener('click', e => {
-	e.preventDefault();
-	var tarea = document.getElementById('nuevatarea').value;
-	var prioridad = document.getElementById('prioridad').value;
+	e.preventDefault()
+	var nombreTarea = tituloTarea.value
+	var prioridadTarea = prioridad.value
 
-	if (tarea.length == 0 || prioridad.length == 0 || tarea[0] == " " || prioridad[0] == " ") {
-		document.getElementById('mensaje').innerText = "Los campos no pueden estar vacios";
-		document.getElementById('form').reset();
+	if (nombreTarea == "" || prioridadTarea == "nulo" || nombreTarea[0] == " ") {
+		console.log(nombreTarea)
+		mensaje.innerText = 'No es posible guardar la tarea, rellene todos los campos'
+		/* document.getElementById('form').reset() */
 
+	} else {
+		mensaje.innerText = ''
+		guardarDatos(nombreTarea, prioridadTarea)
+		/* document.getElementById('form').reset() */
 	}
-	else {
-
-		guardarDatos(tarea, prioridad);
-		document.getElementById('mensaje').innerText = "";
-		document.getElementById('form').reset();
-	}
+	document.querySelector('#form').reset();
 })
 
 function guardarDatos(pTarea, pPrioridad) {
@@ -80,7 +73,7 @@ function guardarDatos(pTarea, pPrioridad) {
 function pintarTarea(pObjeto) {
 	var article = document.createElement('article');
 	var h2 = document.createElement('h2');
-	var textoInterior = document.createTextNode(`${pObjeto.tarea}`);
+	var textoInterior = document.createTextNode(`${pObjeto.pTarea}`);
 
 	h2.appendChild(textoInterior);
 	article.appendChild(h2);
